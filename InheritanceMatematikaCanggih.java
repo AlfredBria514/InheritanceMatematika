413be1f6650c193cd80337fb2624b995825b9759
Alfredbria21 <alfredbria21@gmail.com>
2024-11-24 19:27:59 +0700

    Add files via upload

---
+++ InheritanceMatematikaCanggih.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Object;
+
+/**
+ *
+ * @author tweet
+ */
+public class InheritanceMatematikaCanggih extends InheritanceMatematika{
+    void modulus (int a, int b){
+        hasil = a % b;
+        System.out.println("Hasil Modulus: "+hasil);
+    }
+}
